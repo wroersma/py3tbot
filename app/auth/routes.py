6f069b2dfd272a6f552c72faa0ac892a64c056c5
Wyatt Roersma <wyattroersma@gmail.com>
2018-02-24 17:48:13 -0700

    add select view nav options based on if user is in an admin group(which can work for other groups)

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,12 +1,18 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, session
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_babel import _
 from app import db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm
-from app.models import User
+from app.models import User, Group
 from app.auth.email import send_password_reset_email
+
+
+def get_groups():
+    user_groups = Group.query.filter_by(username=current_user.username).first_or_404()
+    session['groups'] = user_groups.group_name
+    return session['groups']
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -19,8 +25,10 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash(_('Invalid username or password'))
             return redirect(url_for('auth.login'))
+
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
+        get_groups()
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('main.index')
         return redirect(next_page)
